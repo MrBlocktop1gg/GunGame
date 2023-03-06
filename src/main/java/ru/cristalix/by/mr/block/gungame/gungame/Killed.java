01de000aa91ada5209a94cda119b2df8d0854dde
MrBlocktop1gg <mrblockgg1234@yandex.ru>
2023-03-06 20:13:27 +0300

    Merge branch 'GunGame' of https://github.com/MrBlocktop1gg/GunGame into GunGame

--- src/main/java/ru/cristalix/by/mr/block/gungame/gungame/Killed.java
+++ src/main/java/ru/cristalix/by/mr/block/gungame/gungame/Killed.java
@@ -5,12 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -30,44 +27,40 @@ public class Killed implements Listener {
 
     @EventHandler
     public void addItemsKilled(EntityDeathEvent event) {
+        Inventory inventory = event.getEntity().getKiller().getInventory();
         Player player = event.getEntity().getKiller();
         PlayerInventory playerInventory = player.getInventory();
         switch (playerInventory.getItem(0).getType()) {
             case WOOD_SWORD:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.STONE_PICKAXE));
+                inventory.setItem(0, new ItemStack(Material.STONE_PICKAXE));
                 break;
             case STONE_PICKAXE:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.STONE_SWORD));
+                inventory.setItem(0, new ItemStack(Material.STONE_SWORD));
                 break;
             case STONE_SWORD:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.GOLD_AXE));
+                inventory.setItem(0, new ItemStack(Material.GOLD_AXE));
                 break;
             case GOLD_AXE:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.GOLD_SWORD));
+                inventory.setItem(0, new ItemStack(Material.GOLD_SWORD));
             case GOLD_SWORD:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.BOW));
-                event.getEntity().getKiller().getInventory().setItem(26, new ItemStack(Material.ARROW));
-                break;
-            case BOW:
-                event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.IRON_AXE));
+                inventory.setItem(0, new ItemStack(Material.IRON_AXE));
                 break;
             case IRON_AXE:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.IRON_SWORD));
+                inventory.setItem(0, new ItemStack(Material.IRON_SWORD));
                 break;
             case IRON_SWORD:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.DIAMOND_PICKAXE));
+                inventory.setItem(0, new ItemStack(Material.DIAMOND_PICKAXE));
                 break;
             case DIAMOND_PICKAXE:
                 event.getEntity().getKiller().getInventory().clear();
-                event.getEntity().getKiller().getInventory().setItem(0, new ItemStack(Material.DIAMOND_SWORD));
+                inventory.setItem(0, new ItemStack(Material.DIAMOND_SWORD));
             default:
         }
     }
